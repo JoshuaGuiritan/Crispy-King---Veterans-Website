c4eae8150c0fa737a0c8fd28390f6a1ee9ec96ed
Joshua Guiritan <joshuaguiritan05@gmail.com>
2025-06-17 15:01:51 +0800

    eyoo

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,6 +1,6 @@
 const about = ({about}) => {
     return(
-        <div ref={about} className="w-screen h-screen bg-ck-red flex flex-col-reverse md:flex-row justify-center items-center transition-all duration-1000">
+        <div ref={about} className="w-screen h-screen bg-[radial-gradient(circle_at_top_left,_#da2113,_#b50000)] flex flex-col-reverse md:flex-row justify-center items-center transition-all duration-1000">
             <img src="/assets/Images/ckopening.png" className="border border-white xl:w-119 lg:w-110 md:w-100 sm:w-100 w-80 md:mr-12 md:mt-0 mt-5 transition-all duration-500"/>
             <div className="xl:w-119 lg:w-90 md:w-75 sm:w-100 w-82 transition-all duration-500 flex flex-col items-center md:items-start md:justify-start">
                 <div className="relative w-full hidden">
