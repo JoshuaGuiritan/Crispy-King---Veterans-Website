d688365c1111e64f4de037abeeaee4f966026565
Joshua Guiritan <joshuaguiritan05@gmail.com>
2025-06-19 02:14:51 +0800

    eyoo

--- src/sections/products.jsx
+++ src/sections/products.jsx
@@ -11,59 +11,57 @@ const products = ({products}) => {
 
     return(
         <div ref={products} className="w-screen h-screen flex flex-col bg-[#f4f4f4] justify-center items-center">
-            <h1 className="font-milk-cursive mb-10 xl:text-9xl lg:text-8xl text-7xl bg-clip-text text-transparent bg-gradient-to-tl from-[#eec54b] to-[#ffb300] transition-all duration-500">Products</h1>
-            <Carousel className="xl:w-5xl lg:w-4xl w-xs transition-all duration-500" opts={{
-        align: "center",
-      }}>
-                <CarouselContent className="h-[395px]">
-                    <CarouselItem className="">
-                        <div className="p-1">
-                        <Card className="shadow-sm">
-                            <CardContent className="flex aspect-square items-center justify-center p-6">
+            <h1 className="font-milk-cursive mb-5 xl:text-9xl lg:text-8xl text-7xl bg-clip-text text-transparent bg-gradient-to-tl from-[#eec54b] to-[#ffb300] transition-all duration-500">Products</h1>
+            <Carousel className="xl:w-5xl lg:w-4xl w-60 transition-all duration-500">
+                <CarouselContent className="flex justify-center items-center">
+                    <CarouselItem className="h-[300px]">
+                        <div className="">
+                        <Card className="">
+                            <CardContent className="flex aspect-square items-center justify-center">
                             <span className="text-4xl font-semibold"></span>
                             </CardContent>
                         </Card>
                         </div>
                     </CarouselItem>
                     <CarouselItem className="">
-                        <div className="p-1">
+                        <div className="">
                         <Card className="shadow-sm">
-                            <CardContent className="flex aspect-square items-center justify-center p-6">
+                            <CardContent className="flex aspect-square items-center justify-center">
                             <span className="text-4xl font-semibold"></span>
                             </CardContent>
                         </Card>
                         </div>
                     </CarouselItem>
                     <CarouselItem className="">
-                        <div className="p-1">
+                        <div className="">
                         <Card className="shadow-sm">
-                            <CardContent className="flex aspect-square items-center justify-center p-6">
+                            <CardContent className="flex aspect-square items-center justify-center">
                             <span className="text-4xl font-semibold"></span>
                             </CardContent>
                         </Card>
                         </div>
                     </CarouselItem>
                     <CarouselItem className="">
-                        <div className="p-1">
+                        <div className="">
                         <Card className="shadow-sm">
-                            <CardContent className="flex aspect-square items-center justify-center p-6">
+                            <CardContent className="flex aspect-square items-center justify-center">
                             <span className="text-4xl font-semibold"></span>
                             </CardContent>
                         </Card>
                         </div>
                     </CarouselItem>
                     <CarouselItem className="">
-                        <div className="p-1">
+                        <div className="">
                         <Card className="shadow-sm">
-                            <CardContent className="flex aspect-square items-center justify-center p-6">
+                            <CardContent className="flex aspect-square items-center justify-center">
                             <span className="text-4xl font-semibold"></span>
                             </CardContent>
                         </Card>
                         </div>
                     </CarouselItem>
                 </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext/>
+                <CarouselPrevious className="ml-3 w-7 h-7"/>
+                <CarouselNext className="mr-3 w-7 h-7"/>
             </Carousel>
         </div>
     );
