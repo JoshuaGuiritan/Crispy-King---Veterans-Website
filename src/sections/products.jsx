58d7a8b319cbee8049a79f2fa28d204dc9297f9b
Joshua Guiritan <joshuaguiritan05@gmail.com>
2025-06-19 09:14:39 +0800

    eyoo

--- src/sections/products.jsx
+++ src/sections/products.jsx
@@ -10,58 +10,49 @@ import {
 const products = ({products}) => {
 
     return(
-        <div ref={products} className="w-screen h-screen flex flex-col bg-[#f4f4f4] justify-center items-center">
-            <h1 className="font-milk-cursive mb-5 xl:text-9xl lg:text-8xl text-7xl bg-clip-text text-transparent bg-gradient-to-tl from-[#eec54b] to-[#ffb300] transition-all duration-500">Products</h1>
+        <div ref={products} className="w-screen h-screen flex flex-col bg-[#282828] justify-center items-center">
+            <h1 className="font-milk-cursive mb-5 xl:text-9xl lg:text-8xl text-7xl bg-clip-text text-transparent bg-gradient-to-tl from-[#ffd966] to-[#ce9000] transition-all duration-500">Products</h1>
             <Carousel className="xl:w-5xl lg:w-4xl w-60 transition-all duration-500">
-                <CarouselContent className="flex justify-center items-center">
-                    <CarouselItem className="h-[300px]">
-                        <div className="">
-                        <Card className="">
+                <CarouselContent className="flex justify-start items-center">
+                    <CarouselItem className="">
+                        <Card className="bg-[#404040] border-none">
                             <CardContent className="flex aspect-square items-center justify-center">
-                            <span className="text-4xl font-semibold"></span>
+                            <span className="text-4xl font-semibold text-white">1</span>
                             </CardContent>
                         </Card>
-                        </div>
                     </CarouselItem>
                     <CarouselItem className="">
-                        <div className="">
-                        <Card className="shadow-sm">
+                        <Card className="bg-[#404040] border-none">
                             <CardContent className="flex aspect-square items-center justify-center">
-                            <span className="text-4xl font-semibold"></span>
+                            <span className="text-4xl font-semibold text-white">2</span>
                             </CardContent>
                         </Card>
-                        </div>
+                        
                     </CarouselItem>
                     <CarouselItem className="">
-                        <div className="">
-                        <Card className="shadow-sm">
+                        <Card className="bg-[#404040] border-none">
                             <CardContent className="flex aspect-square items-center justify-center">
-                            <span className="text-4xl font-semibold"></span>
+                            <span className="text-4xl font-semibold text-white">3</span>
                             </CardContent>
                         </Card>
-                        </div>
                     </CarouselItem>
                     <CarouselItem className="">
-                        <div className="">
-                        <Card className="shadow-sm">
+                        <Card className="bg-[#404040] border-none">
                             <CardContent className="flex aspect-square items-center justify-center">
-                            <span className="text-4xl font-semibold"></span>
+                            <span className="text-4xl font-semibold text-white">4</span>
                             </CardContent>
                         </Card>
-                        </div>
                     </CarouselItem>
                     <CarouselItem className="">
-                        <div className="">
-                        <Card className="shadow-sm">
+                        <Card className="bg-[#404040] border-none">
                             <CardContent className="flex aspect-square items-center justify-center">
-                            <span className="text-4xl font-semibold"></span>
+                            <span className="text-4xl font-semibold text-white">5</span>
                             </CardContent>
                         </Card>
-                        </div>
                     </CarouselItem>
                 </CarouselContent>
-                <CarouselPrevious className="ml-3 w-7 h-7"/>
-                <CarouselNext className="mr-3 w-7 h-7"/>
+                <CarouselPrevious className="bg-[#555555] border-none hover:bg-[#4d4c4c] cursor-pointer text-gray-200"/>
+                <CarouselNext className="bg-[#555555] border-none hover:bg-[#4d4c4c] cursor-pointer text-gray-200"/>
             </Carousel>
         </div>
     );
